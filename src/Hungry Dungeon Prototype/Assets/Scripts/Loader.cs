b8f70f53d29560d67bc1614fd2b74196370e6b17
Mark Bridgewater <70918040+MarkoBridgewater@users.noreply.github.com>
2020-09-19 19:44:23 -0400

    Completed Prefabs, got something on the screen (a level) wrote board manager, game manager, and loader scripts. game builds and runs

---
+++ src/Hungry Dungeon Prototype/Assets/Scripts/Loader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+
+    public GameObject gameManager;      //GameManager prefab to instant
+    
+    void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
