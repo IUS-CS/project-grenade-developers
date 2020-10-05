db7fce2fb4aaadba4e63c64d8e50864e15be83b4
Mark Bridgewater <70918040+MarkoBridgewater@users.noreply.github.com>
2020-10-05 17:39:47 -0400

    Started Working on items

---
+++ src/Assets/Scripts/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Sprite itemSprite; //Switches to proper sprite for item 
+    public int itemType; //Determine what the item is
+
+    private SpriteRenderer spriteRenderer; //Store component ref to the attached SpriteRenderer
+
+
+    void Awake()
+    {
+        //Get component ref to SpriteRenderer
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
