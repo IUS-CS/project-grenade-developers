058fb09cbb2bd64eabd78fa09548e9c2191c8149
Nathanael Mann <IAMNameless99@gmail.com>
2020-10-11 04:35:41 -0400

    Implemented Basic Prim Mazes.

--- src/Assets/Scripts/BoardManager.cs
+++ src/Assets/Scripts/BoardManager.cs
@@ -30,7 +30,6 @@ public class BoardManager : MonoBehaviour {
     public GameObject[] enemyTiles; //Array of enemy prefabs
     public GameObject[] itemTiles; //Array of item prefabs
 
-
     private Transform boardHolder; //Reference to the transform of the board object
     private List<Vector3> gridPositions = new List<Vector3>(); //List of possible locations to put tiles such as enemies
 
@@ -51,14 +50,76 @@ public class BoardManager : MonoBehaviour {
     //Setup for random patterns
     private void BoardSetup()
     {
-        boardHolder = new GameObject("Board").transform;
+        //Generates a random mesh for placing walls in the level.
+        // 1 = Wall
+        // 0 = Floor
+        int[,] levelGen = new int[columns,rows];
+        for (int k = 0; k < columns; k++){
+            for(int l = 0; l < rows; l++){
+                levelGen[k, l] = 1; //start with a grid full of walls.
+            }
+        }
+        levelGen[0, 0] = 0; //starting empty cell
+        List<int> wallList = new List<int>(){1,0,0,1}; //initilize wallList with first neighbors
+        
+        while(wallList.Count > 0){
+            // select a random wall to be a potential new path
+            int randomWall = 2 * Random.Range(0, ((wallList.Count)/2));
+            int newWallX = wallList[randomWall];
+            int newWallY = wallList[randomWall+1];
+            
+            //now test if the new wall could become a valid path
+            int connectedPaths = 0;
+            if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 0 )
+                connectedPaths++;
+            if (newWallX+1 <= 19 && levelGen[newWallX+1,newWallY] == 0 )
+                connectedPaths++;
+            if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 0 )
+                connectedPaths++;
+            if (newWallY+1 <= 19 && levelGen[newWallX,newWallY+1] == 0 )
+                connectedPaths++;
+            if (newWallX-1 >= 0 && newWallY-1 >= 0 && levelGen[newWallX-1,newWallY-1] == 0 )
+                connectedPaths++;
+            if (newWallX+1 <= 19 && newWallY-1 >= 0 && levelGen[newWallX+1,newWallY-1] == 0 )
+                connectedPaths++;
+            if (newWallX-1 >= 0 && newWallY+1 <= 19 && levelGen[newWallX-1,newWallY+1] == 0 )
+                connectedPaths++;
+            if (newWallX+1 <= 19 && newWallY+1 <= 19 && levelGen[newWallX+1,newWallY+1] == 0 )
+                connectedPaths++;
 
+            if (connectedPaths == 1 || connectedPaths == 2) {
+
+                levelGen[newWallX,newWallY] = 0; //turn the wall into a path
+                
+                //add the new neighbors to wallList
+                if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 1 ){
+                    wallList.Add(newWallX-1);
+                    wallList.Add(newWallY);
+                }
+                if (newWallX+1 <= 19 && levelGen[newWallX+1,newWallY] == 1 ){
+                    wallList.Add(newWallX+1);
+                    wallList.Add(newWallY);
+                }
+                if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 1 ){
+                    wallList.Add(newWallX);
+                    wallList.Add(newWallY-1);
+                }
+                if (newWallY+1 <= 19 && levelGen[newWallX,newWallY+1] == 1 ){
+                    wallList.Add(newWallX);
+                    wallList.Add(newWallY+1);
+                }
+            }
+            wallList.RemoveAt(randomWall); //remove the wall from the list
+            wallList.RemoveAt(randomWall); //remove the wall from the list
+        }//while
+        
+        boardHolder = new GameObject("Board").transform;
         for(int i = -1; i < columns + 1; i++)
         {
             for(int j = -1; j < rows + 1; j++)
             {
                 GameObject toCreate = floorTiles[Random.Range(0, floorTiles.Length)];
-                if (i == -1 || i == columns || j == -1 || j == rows)
+                if (i == -1 || i == columns || j == -1 || j == rows || levelGen[i, j] == 1)
                     toCreate = wallTiles[Random.Range(0, wallTiles.Length)];
                 GameObject instance = Instantiate(toCreate, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
 
