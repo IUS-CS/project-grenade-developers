1c0f78c4ae0651975205bbaca527afb475aaccd4
Nathanael Mann <IAMNameless99@gmail.com>
2020-10-18 16:10:44 -0400

    Iplemented more natural looking terrain.

--- src/Assets/Scripts/BoardManager.cs
+++ src/Assets/Scripts/BoardManager.cs
@@ -56,10 +56,10 @@ public class BoardManager : MonoBehaviour {
         int[,] levelGen = new int[columns,rows];
         for (int k = 0; k < columns/5; k++){
             for(int l = 0; l < rows/5; l++){
-                levelGen[k, l] = 1; //start with a grid full of walls.
+                levelGen[k, l] = 0; //start with a grid full of walls.
             }
         }
-        levelGen[0, 0] = 0; //starting empty cell
+        levelGen[0, 0] = 1; //starting empty cell
         List<int> wallList = new List<int>(){1,0,0,1}; //initilize wallList with first neighbors
         
         while(wallList.Count > 0){
@@ -70,41 +70,41 @@ public class BoardManager : MonoBehaviour {
             
             //now test if the new wall could become a valid path
             int connectedPaths = 0;
-            if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 0 )
+            if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 1 )
                 connectedPaths++;
-            if (newWallX+1 <= (rows-1)/5 && levelGen[newWallX+1,newWallY] == 0 )
+            if (newWallX+1 <= (rows-1)/5 && levelGen[newWallX+1,newWallY] == 1 )
                 connectedPaths++;
-            if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 0 )
+            if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 1 )
                 connectedPaths++;
-            if (newWallY+1 <= (columns-1)/5 && levelGen[newWallX,newWallY+1] == 0 )
+            if (newWallY+1 <= (columns-1)/5 && levelGen[newWallX,newWallY+1] == 1 )
                 connectedPaths++;
-            if (newWallX-1 >= 0 && newWallY-1 >= 0 && levelGen[newWallX-1,newWallY-1] == 0 )
+            if (newWallX-1 >= 0 && newWallY-1 >= 0 && levelGen[newWallX-1,newWallY-1] == 1 )
                 connectedPaths++;
-            if (newWallX+1 <= (rows-1)/5 && newWallY-1 >= 0 && levelGen[newWallX+1,newWallY-1] == 0 )
+            if (newWallX+1 <= (rows-1)/5 && newWallY-1 >= 0 && levelGen[newWallX+1,newWallY-1] == 1 )
                 connectedPaths++;
-            if (newWallX-1 >= 0 && newWallY+1 <= (columns-1)/5 && levelGen[newWallX-1,newWallY+1] == 0 )
+            if (newWallX-1 >= 0 && newWallY+1 <= (columns-1)/5 && levelGen[newWallX-1,newWallY+1] == 1 )
                 connectedPaths++;
-            if (newWallX+1 <= (rows-1)/5 && newWallY+1 <= (columns-1)/5 && levelGen[newWallX+1,newWallY+1] == 0 )
+            if (newWallX+1 <= (rows-1)/5 && newWallY+1 <= (columns-1)/5 && levelGen[newWallX+1,newWallY+1] == 1 )
                 connectedPaths++;
 
             if (connectedPaths == 1 || connectedPaths == 2) {
 
-                levelGen[newWallX,newWallY] = 0; //turn the wall into a path
+                levelGen[newWallX,newWallY] = 1; //turn the wall into a path
                 
                 //add the new neighbors to wallList
-                if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 1 ){
+                if (newWallX-1 >= 0 && levelGen[newWallX-1,newWallY] == 0){
                     wallList.Add(newWallX-1);
                     wallList.Add(newWallY);
                 }
-                if (newWallX+1 <= rows-1 && levelGen[newWallX+1,newWallY] == 1 ){
+                if (newWallX+1 <= rows-1 && levelGen[newWallX+1,newWallY] == 0){
                     wallList.Add(newWallX+1);
                     wallList.Add(newWallY);
                 }
-                if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 1 ){
+                if (newWallY-1 >= 0 && levelGen[newWallX,newWallY-1] == 0){
                     wallList.Add(newWallX);
                     wallList.Add(newWallY-1);
                 }
-                if (newWallY+1 <= columns-1 && levelGen[newWallX,newWallY+1] == 1 ){
+                if (newWallY+1 <= columns-1 && levelGen[newWallX,newWallY+1] == 0){
                     wallList.Add(newWallX);
                     wallList.Add(newWallY+1);
                 }
@@ -113,13 +113,170 @@ public class BoardManager : MonoBehaviour {
             wallList.RemoveAt(randomWall); //remove the wall from the list
         }//while
         
+        //redefine levelGen tiles based on neightbors.
+        int[,] levelGen2 = new int[columns/5,rows/5];
+        for (int k = 0; k < columns/5; k++){
+            for(int l = 0; l < rows/5; l++){
+                 
+                if (levelGen[k,l] == 1){
+
+                    int cellRight = (k == (columns/5)-1) ? 0 : levelGen[k+1,l];
+                    int cellLeft = (k == 0) ? 0 : levelGen[k-1,l];
+                    int cellUp = (l == (rows/5)-1) ? 0 : levelGen[k,l+1];
+                    int cellDown = (l == 0) ? 0 : levelGen[k,l-1];
+
+                    //cell has neighbors up and left
+                    if (cellUp == 1 && cellDown == 0 && cellRight == 0 && cellLeft == 1) {
+                        levelGen2[k,l] = 1;
+                    }
+                    //cell has neighbors up and right
+                    else if (cellUp == 1 && cellDown == 0 && cellRight == 1 && cellLeft == 0) {
+                        levelGen2[k,l] = 2;
+                    }
+                    //cell has neighbors down and right
+                    else if (cellUp == 0 && cellDown == 1 && cellRight == 1 && cellLeft == 0) {
+                        levelGen2[k,l] = 3;
+                    }
+                    //cell has neighbors down and left
+                    else if (cellUp == 0 && cellDown == 1 && cellRight == 0 && cellLeft == 1) {
+                        levelGen2[k,l] = 4;
+                    }
+                    //cell has neighbors left and right
+                    else if (cellUp == 0 && cellDown == 0 && cellRight == 1 && cellLeft == 1) {
+                        levelGen2[k,l] = 5;
+                    }
+                    //cell has neighbors up and down
+                    else if (cellUp == 1 && cellDown == 1 && cellRight == 0 && cellLeft == 0) {
+                        levelGen2[k,l] = 6;
+                    }
+                    //cell has neighbor only left
+                    else if (cellUp == 0 && cellDown == 0 && cellRight == 0 && cellLeft == 1) {
+                        levelGen2[k,l] = 7;
+                    }
+                    //cell has neighbor only up
+                    else if (cellUp == 1 && cellDown == 0 && cellRight == 0 && cellLeft == 0) {
+                        levelGen2[k,l] = 8;
+                    }
+                    //cell has neighbor only right
+                    else if (cellUp == 0 && cellDown == 0 && cellRight == 1 && cellLeft == 0) {
+                        levelGen2[k,l] = 9; 
+                    }
+                    //cell has neighbor only down
+                    else if (cellUp == 0 && cellDown == 1 && cellRight == 0 && cellLeft == 0) {
+                        levelGen2[k,l] = 10;
+                    } else {
+                    //ELSE cell has different pattern of neighbors:
+                        levelGen2[k,l] = 11;
+                    }
+                } else {
+                    levelGen2[k,l] = 0; //otherwise it's a wall
+                }//if-else
+            }//for l
+        }//for k
+        
         //initialize levelGenRooms as a scale-up of levelgen
         int[,] levelGenRooms = new int[columns,rows];
-        for (int k = 0; k < columns; k++){
-            for(int l = 0; l < rows; l++){
-                levelGenRooms[k, l] = levelGen[k/5,l/5]; //start with a grid full of walls.
-            }
-        }
+        int[,] room =  new int[5,5];
+        for (int k = 0; k < columns/5; k++){
+            for(int l = 0; l < rows/5; l++){
+                
+                switch(levelGen2[k,l]) {
+                //THESE ROOMS ARE ROTATED 90 DEGREES CLOCKWISE OF HOW THEY WILL APPEAR IN GAME
+                    case 0:
+                        room = new int[,] { {0,0,0,0,0}, 
+                                            {0,0,0,0,0}, 
+                                            {0,0,0,0,0}, 
+                                            {0,0,0,0,0}, 
+                                            {0,0,0,0,0} };
+                        break;
+                    case 1:
+                        room = new int[,] { {1,1,1,1,1}, 
+                                            {1,1,1,1,1}, 
+                                            {0,1,1,1,1},
+                                            {0,0,1,1,1},
+                                            {0,0,0,1,1} };
+                        break;
+                    case 2:
+                        room = new int[,] { {0,0,0,1,1}, 
+                                            {0,0,1,1,1}, 
+                                            {0,1,1,1,1},
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1} };
+                        break;
+                    case 3:
+                        room = new int[,] { {1,1,0,0,0}, 
+                                            {1,1,1,0,0}, 
+                                            {1,1,1,1,0},
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1} };
+                        break;
+                    case 4:
+                        room = new int[,] { {1,1,1,1,1}, 
+                                            {1,1,1,1,1}, 
+                                            {1,1,1,1,0},
+                                            {1,1,1,0,0},
+                                            {1,1,0,0,0} };
+                        break;
+                    case 5:
+                        room = new int[,] { {1,1,1,1,1}, 
+                                            {0,1,1,1,0}, 
+                                            {0,1,1,1,0},
+                                            {0,1,1,1,0},
+                                            {1,1,1,1,1} };
+                        break;
+                    case 6:
+                        room = new int[,] { {1,0,0,0,1}, 
+                                            {1,1,1,1,1}, 
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1},
+                                            {1,0,0,0,1} };
+                        break;
+                    case 7:
+                        room = new int[,] { {1,1,1,1,1}, 
+                                            {1,1,1,1,1}, 
+                                            {0,1,1,1,0},
+                                            {0,1,1,1,0},
+                                            {0,0,1,0,0} };
+                        break;
+                    case 8:
+                        room = new int[,] { {0,0,0,1,1}, 
+                                            {0,1,1,1,1}, 
+                                            {1,1,1,1,1},
+                                            {0,1,1,1,1},
+                                            {0,0,0,1,1} };
+                        break;
+                    case 9:
+                        room = new int[,] { {0,0,1,0,0}, 
+                                            {0,1,1,1,0}, 
+                                            {0,1,1,1,0},
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1} };
+                        break;
+                    case 10:
+                        room = new int[,] { {1,1,0,0,0}, 
+                                            {1,1,1,1,0}, 
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,0},
+                                            {1,1,0,0,0} };
+                        break;
+                    case 11:
+                        room = new int[,] { {1,1,1,1,1}, 
+                                            {1,1,1,1,1}, 
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1},
+                                            {1,1,1,1,1} };
+                        break;
+   
+                }//switch
+                
+                for(int k2 = k*5; k2 < (k*5)+5; k2++){
+                    for(int l2 = l*5; l2 < (l*5)+5; l2++){
+                        levelGenRooms[k2,l2] = room[k2%5,l2%5];
+                    }//for l2
+                }//for k2
+                
+            }//for l
+        }//for k
         
         boardHolder = new GameObject("Board").transform;
         for(int i = -5; i < columns + 5; i++)
@@ -127,7 +284,7 @@ public class BoardManager : MonoBehaviour {
             for(int j = -5; j < rows + 5; j++)
             {
                 GameObject toCreate = floorTiles[Random.Range(0, floorTiles.Length)];
-                if (i <= -1 || i >= columns || j <= -1 || j >= rows || levelGenRooms[i, j] >= 1)
+                if (i <= -1 || i >= columns || j <= -1 || j >= rows || levelGenRooms[i, j] == 0)
                     toCreate = wallTiles[Random.Range(0, wallTiles.Length)];
                 GameObject instance = Instantiate(toCreate, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
 
@@ -163,8 +320,8 @@ public class BoardManager : MonoBehaviour {
     {
         BoardSetup();
         InitialiseList();
-        int enemyCount = 2;
-        int itemCount = 3;
+        int enemyCount = 0;
+        int itemCount = 0;
         LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
         LayoutObjectAtRandom(itemTiles, itemCount, itemCount);
     }
